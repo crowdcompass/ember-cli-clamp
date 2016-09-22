85da0573c959557dc1194653e32cbc93f9f6c90f
Renae Corcoran <rcorcoran@crowdcompass.com>
2016-09-22 09:55:28 -0700

    add dependencies

--- addon/components/clamp-js.js
+++ addon/components/clamp-js.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-import layout from '../templates/components/clamp-js';\
+import layout from '../templates/components/clamp-js';
 
 /**
  * Clamp.js Component
--- index.js
+++ index.js
@@ -5,6 +5,8 @@ module.exports = {
   name: 'ember-cli-clamp',
   included: function(app) {
     this._super.included(app);
-    app.import(app.bowerDirectory + '/clamp-js/clamp.min.js');
+    this.app.import(
+      app.bowerDirectory + '/clamp-js/clamp.min.js'
+    );
   }
 };
